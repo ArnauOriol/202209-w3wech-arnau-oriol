ff010a373fefef3bb9b95aed140ff82e5512747c
Arnau Oriol <arnauoriol1996@hotmail.com>
2022-10-15 17:31:14 +0200

    add comented fetchPokemonList test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+import fetchPokemonList from "./api-connectors/fetchPokemonList/fetchPokemonList.js";
+import {
+  pokemonApiUrl,
+  pokemonListLimit,
+  offset,
+} from "./api-connectors/urls.js";
+import { PokemonNameAndUrl } from "./types.js";
 
+const pokemonList = await fetchPokemonList(
+  pokemonApiUrl,
+  pokemonListLimit,
+  offset
+);
+
+const getPokemonInfo = async () => {};
